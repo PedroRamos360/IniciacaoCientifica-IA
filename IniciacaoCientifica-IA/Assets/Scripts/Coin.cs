bdcfcc68b2fb7f8b2241a12da5f617cf74767831
Pedro Ramos <ramosct70@hotmail.com>
2020-03-07 19:59:35 -0300

    Atualização

---
+++ IniciacaoCientifica-IA/Assets/Scripts/Coin.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Player") {
+            transform.position = new Vector3(Random.Range(-11.5f, 11.5f), Random.Range(4.50f, -2.13f), -0.06579258f);
+        }
+    }
+}
---
+++ IniciacaoCientifica-IA/Assets/Scripts/MovimentoJogador.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentoJogador : MonoBehaviour {
+    [SerializeField]
+    float speed = 1f;
+
+    void Update() {
+        // Corrige a velocidade quando se movendo na diagonal
+        if (Input.GetAxisRaw("Horizontal") != 0 && Input.GetAxisRaw("Vertical") != 0) {
+            Vector3 movementXY = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0f);
+            transform.position += movementXY * Time.deltaTime * speed;
+        } else {
+            // Verifica o movimento horizontal
+            Vector3 movementX = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+            transform.position += movementX * Time.deltaTime * speed;
+
+            // Verifica o movimento vertical
+            Vector3 movementY = new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+            transform.position += movementY * Time.deltaTime * speed;
+        }
+    }
+}
